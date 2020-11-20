8698d2ff1af60fc44eff1f2c011b41901e63fec8
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-11-20 15:19:26 +0500

    Время разбивать камни

--- src/view/trip-tabs.js
+++ src/view/trip-tabs.js
@@ -1,5 +1,6 @@
 export const createTripTabsTemplate = () => {
-  return `<nav class="trip-controls__trip-tabs  trip-tabs">
+  return `<h2 class="visually-hidden">Switch trip view</h2>
+  <nav class="trip-controls__trip-tabs  trip-tabs">
     <a class="trip-tabs__btn" href="#">Table</a>
     <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Stats</a>
   </nav>`;
