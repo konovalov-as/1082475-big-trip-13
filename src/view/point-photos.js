e386f4c1b7b954565e63d6ceb3439d4b40392c60
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-12-07 11:23:12 +0500

    Добавляет изменения в отрисовку фотографий

--- src/view/point-photos.js
+++ src/view/point-photos.js
@@ -1,18 +1,19 @@
 import AbstractView from './abstract';
 
 const createPointPhotosTemplate = (point) => {
-  const photos = point.destinationInfo[0].photos;
-  let photosList = ``;
-  const createPhoto = () => {
-    for (const photo of photos) {
-      photosList += `<img class="event__photo" src="${photo}" alt="Event photo">`;
-    }
-    return photosList;
+  const createPhotoTemplate = (photo) => {
+    return `<img class="event__photo" src="${photo}" alt="Event photo">`;
   };
+
+  const photos = point.destinationInfo[0].photos;
+
+  let photosList = photos
+  .map((photo) => createPhotoTemplate(photo))
+  .join(``);
 
   return `<div class="event__photos-container">
     <div class="event__photos-tape">
-    ${createPhoto()}
+    ${photosList}
     </div>
   </div>`;
 };
