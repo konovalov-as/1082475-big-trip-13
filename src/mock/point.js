ca2dde95ab4a00c3082d2bc9c5d0974f526c1502
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-12-13 10:11:32 +0500

    Добавляет изменения в генерацию предложений и описания точки маршрута

--- src/mock/point.js
+++ src/mock/point.js
@@ -29,7 +29,7 @@ const generateOffer = () => {
   };
 };
 
-const generateOffers = () => {
+export const generateOffers = () => {
   const offersCount = getRandomInteger(0, 5);
   const offers = [];
 
@@ -61,7 +61,13 @@ const generateDate = () => {
   return dayjs().add(daysGap, `day`).add(hoursGap, `hours`).add(minutesGap, `minutes`);
 };
 
-export const generateOptions = generateOffers;
+const descriptions = [`Lorem ipsum dolor sit amet, consectetur adipiscing elit.`, `Cras aliquet varius magna, non porta ligula feugiat eget.`, `Fusce tristique felis at fermentum pharetra.`, `Aliquam id orci ut lectus varius viverra.`, `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`];
+
+export const generateDescription = () => {
+  const descriptionsCount = getRandomInteger(1, 5);
+
+  return descriptions.slice(0, descriptionsCount).join(``);
+};
 
 export const generatePoint = () => {
   const dateTimeStartEvent = generateDate();
@@ -77,7 +83,7 @@ export const generatePoint = () => {
     destinationCity: generateDestinationCity(),
     offers: generateOffers(),
     destinationInfo: [{
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`,
+      description: generateDescription(),
       photos: generatePhotos(),
     }],
     dateTimeStartEvent,
