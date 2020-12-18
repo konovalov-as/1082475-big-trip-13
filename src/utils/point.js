338229a3858bcfb8ef8b24058f6e1032df337f81
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-12-18 15:17:33 +0500

    Добавляет функции сортировки точки без даты

---
+++ src/utils/point.js
@@ -0,0 +1,38 @@
+import dayjs from 'dayjs';
+// Функция помещает точки без даты в конце списка,
+// возвращая нужный вес для колбэка sort
+const getWeightForNullDate = (dateA, dateB) => {
+  if (dateA === null && dateB === null) {
+    return 0;
+  }
+
+  if (dateA === null) {
+    return 1;
+  }
+
+  if (dateB === null) {
+    return -1;
+  }
+
+  return null;
+};
+
+export const sortPointOlder = (pointA, pointB) => {
+  const weight = getWeightForNullDate(pointA.dueDate, pointB.dueDate);
+
+  if (weight !== null) {
+    return weight;
+  }
+
+  return dayjs(pointA.dueDate).diff(dayjs(pointB.dueDate));
+};
+
+export const sortPointNewer = (pointA, pointB) => {
+  const weight = getWeightForNullDate(pointA.dueDate, pointB.dueDate);
+
+  if (weight !== null) {
+    return weight;
+  }
+
+  return dayjs(pointB.dueDate).diff(dayjs(pointA.dueDate));
+};
