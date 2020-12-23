6cc3a765c03c2ddad75c44f16db89bc10dae8204
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-12-23 12:26:14 +0500

    Добавляет адаптер от сервера к клиенту и обратно

--- src/model/points.js
+++ src/model/points.js
@@ -53,4 +53,71 @@ export default class Points extends Observer {
 
     this._notify(updateType);
   }
+
+  static adaptToClient(point) {
+    const adaptedPoint = Object.assign(
+        {},
+        point,
+        {
+          pointType: point.type,
+          destinationCity: point.destination.name,
+          offers: {
+            condition: point.offers.title,
+            cost: point.offers.price,
+          },
+          destinationInfo: {
+            description: point.destination.description,
+            photos: point.destination.picture,
+          },
+          dateTimeStartEvent: point.date_from,
+          dateTimeEndEvent: point.date_to,
+          cost: point.base_price,
+          isFavorite: point.is_favorite,
+        }
+    );
+
+    delete adaptedPoint.type;
+    delete adaptedPoint.destination;
+    delete adaptedPoint.offers;
+    delete adaptedPoint.date_from;
+    delete adaptedPoint.date_to;
+    delete adaptedPoint.base_price;
+    delete adaptedPoint.is_favorite;
+
+    return adaptedPoint;
+  }
+
+  static adaptToServer(point) {
+    const adaptedPoint = Object.assign(
+        {},
+        point,
+        {
+          'base_price': point.cost,
+          'date_from': point.dateTimeStartEvent instanceof Date ? point.dateTimeEndEvent.toISOString() : null,
+          'date_to': point.dateTimeEndEvent instanceof Date ? point.dateTimeEndEvent.toISOString() : null,
+          'destination': {
+            name: point.destinationCity,
+            description: point.destinationInfo.description,
+            pictures: point.destinationInfo.photos,
+          },
+          'is_favorite': point.isFavorite,
+          'offers': [{
+            price: point.cost,
+            title: point.offers.condition,
+          }],
+          'type': point.pointType,
+        }
+    );
+
+    delete adaptedPoint.cost;
+    delete adaptedPoint.dateTimeStartEvent;
+    delete adaptedPoint.dateTimeEndEvent;
+    delete adaptedPoint.destinationCity;
+    delete adaptedPoint.destinationInfo;
+    delete adaptedPoint.isFavorite;
+    delete adaptedPoint.offers;
+    delete adaptedPoint.pointType;
+
+    return adaptedPoint;
+  }
 }
