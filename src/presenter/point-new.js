02068a4cf72268b9e3e549edae434f749e96c3ba
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2021-01-26 15:57:56 +0500

    Удаляет список городов из констант

--- src/presenter/point-new.js
+++ src/presenter/point-new.js
@@ -1,24 +1,11 @@
 import PointEditView from '../view/point-edit';
+
 import {remove, render, RenderPosition} from '../utils/render';
-import {UserAction, UpdateType, Key, POINT_TYPES, DESTINATION_CITIES} from '../const';
+import {UserAction, UpdateType, Key, POINT_TYPES} from '../const';
 import dayjs from 'dayjs';
 
 import {isOnline} from '../utils/common';
 import {toast} from '../utils/toast/toast.js';
-
-const BLANK_POINT = {
-  pointType: POINT_TYPES[0].toLowerCase(),
-  destinationCity: DESTINATION_CITIES[0],
-  offers: [],
-  destinationInfo: {
-    description: ``,
-    photos: ``,
-  },
-  dateTimeStartEvent: dayjs(),
-  dateTimeEndEvent: dayjs().add(1, `day`),
-  cost: 0,
-  isFavorite: false,
-};
 
 export default class PointNew {
   constructor(pointListContainer, changeData, newEventButton) {
@@ -33,6 +20,21 @@ export default class PointNew {
     this._onDeleteClick = this._onDeleteClick.bind(this);
     this._onCloseClick = this._onCloseClick.bind(this);
     this._onEscKeyDown = this._onEscKeyDown.bind(this);
+
+    this._listCities = [];
+    this._BLANK_POINT = {
+      pointType: POINT_TYPES[0].toLowerCase(),
+      destinationCity: ``,
+      offers: [],
+      destinationInfo: {
+        description: ``,
+        photos: ``,
+      },
+      dateTimeStartEvent: dayjs(),
+      dateTimeEndEvent: dayjs().add(1, `day`),
+      cost: 0,
+      isFavorite: false,
+    };
   }
 
   init(callback, offers, destinations) {
@@ -44,8 +46,9 @@ export default class PointNew {
 
     this._getOffers(offers);
     this._getDestinations(destinations);
+    this._getListCities(destinations);
 
-    this._pointEditComponent = new PointEditView(BLANK_POINT, offers, destinations, this._newEventButton);
+    this._pointEditComponent = new PointEditView(this._BLANK_POINT, offers, destinations, this._newEventButton);
     this._pointEditComponent.setOnFormSubmitClick(this._onFormSubmit);
     this._pointEditComponent.setOnFormDeleteClick(this._onDeleteClick);
     this._pointEditComponent.setOnEditFormClose(this._onCloseClick);
@@ -57,8 +60,8 @@ export default class PointNew {
 
   _getOffers(offers) {
     for (const offer of offers) {
-      if (offer.pointType === (BLANK_POINT.pointType.toLowerCase())) {
-        BLANK_POINT.offers = offer.offers;
+      if (offer.pointType === (this._BLANK_POINT.pointType.toLowerCase())) {
+        this._BLANK_POINT.offers = offer.offers;
         break;
       }
     }
@@ -66,12 +69,19 @@ export default class PointNew {
 
   _getDestinations(destinations) {
     for (const destination of destinations) {
-      if (destination.name === BLANK_POINT.destinationCity) {
-        BLANK_POINT.destinationInfo.description = destination.destinationInfo.description;
-        BLANK_POINT.destinationInfo.photos = this._getPhotos(destination.destinationInfo.photos);
+      if (destination.name === this._BLANK_POINT.destinationCity) {
+        this._BLANK_POINT.destinationInfo.description = destination.destinationInfo.description;
+        this._BLANK_POINT.destinationInfo.photos = this._getPhotos(destination.destinationInfo.photos);
         break;
       }
     }
+  }
+
+  _getListCities(destinations) {
+    destinations.map((destination) => {
+      this._listCities.push(destination.name);
+    });
+    this._BLANK_POINT.destinationCity = this._listCities[0];
   }
 
   _getPhotos(photos) {
